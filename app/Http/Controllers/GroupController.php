682cc23c4abe01f00dd59ad1b0cc88bac12f795a
Erico79 <emurinyo@gmail.com>
2019-01-10 00:26:14 +0300

    update chama application details when user makes changes when navigating back and forth in the application process

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -10,12 +10,34 @@ class GroupController extends Controller
 {
     public function register() {
         $validator = Validator::make(request()->all(), [
-            'name' => 'required|unique:groups',
+            'name' => 'required',
             'no_of_members' => 'required|numeric|min:2'
         ]);
 
+        $validator->sometimes('name', 'unique:groups', function($input) {
+            return !request()->has('group_id');
+        });
+
+        if (request()->has('group_id')) {
+            $group = Group::find(request('group_id'));
+            
+            $validator->after(function ($validator) {
+                $group_exists = Group::where('id', '<>', request('group_id'))->where('name', request('name'))->count();
+
+                if ($group_exists) 
+                    $validator->errors()->add('field', 'Something is wrong with this field!');
+            });
+        }
+
         if (!$validator->fails()) {
-            $group = Group::create(request()->all());
+            if ($group_id = request('group_id')) {
+                Group::where('id', $group_id)->update([
+                    'name' => request('name'), 
+                    'no_of_members' => request('no_of_members')
+                ]);
+            } else {
+                $group = Group::create(request()->all());
+            }
 
             return response(['success' => true, 'message' => 'Your Group has been registered!', 'group' => $group]);
         } else {
