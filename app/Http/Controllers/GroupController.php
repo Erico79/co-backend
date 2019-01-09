c279c32fa8144668ef4270c184a9bc923a38a966
Erico79 <emurinyo@gmail.com>
2019-01-09 21:59:17 +0300

    setup passport

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Group;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GroupController extends Controller
 {
-    //
+    public function register() {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required|unique:groups',
+            'no_of_members' => 'required|numeric|min:2'
+        ]);
+
+        if (!$validator->fails()) {
+            $group = Group::create(request()->all());
+
+            return response(['success' => true, 'message' => 'Your Group has been registered!', 'group' => $group]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->getMessageBag()->toArray(),
+            ]);
+        }
+    }
 }
