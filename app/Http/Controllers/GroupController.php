b503806b284d508d102183958755d9b6c72b4639
Erico79 <emurinyo@gmail.com>
2019-01-13 16:10:33 +0300

    fix bug

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -14,18 +14,23 @@ class GroupController extends Controller
             'no_of_members' => 'required|numeric|min:2'
         ]);
 
-        $validator->sometimes('name', 'unique:groups', function($input) {
-            return !request()->has('group_id');
-        });
+        $group_exists_message = 'A Chama with the same name already exists!';
 
         if (request()->has('group_id')) {
             $group = Group::find(request('group_id'));
 
-            $validator->after(function ($validator) {
+            $validator->after(function ($validator) use($group_exists_message) {
                 $group_exists = Group::where('id', '<>', request('group_id'))->where('name', request('name'))->count();
 
                 if ($group_exists) 
-                    $validator->errors()->add('name', 'Group Name already exists!');
+                    $validator->errors()->add('name', $group_exists_message);
+            });
+        } else {
+            $validator->after(function($validator) use($group_exists_message) {
+                $group_exists = Group::where('name', request('name'))->count();
+
+                if ($group_exists) 
+                    $validator->errors()->add('name', $group_exists_message);
             });
         }
 
