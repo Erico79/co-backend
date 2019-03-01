bdbbecfe6fd4e0c3eda7fc9b64cbbdf97bdd1fc4
Erico79 <emurinyo@gmail.com>
2019-03-01 11:18:10 +0300

    fix all registration api's

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,63 +10,33 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    public function store() {
+        $validator = Validator::make(request()->all(), [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'mobile_phone' => ['required', 'numeric', 'max:255'],
+            'group_id' => ['required', 'numeric'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
-    }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        $validator->after(function($validator) {
+            $admin_email_exists = User::where('group_id', request('group_id')->where('email', request('email')))->count();
+            if ($admin_email_exists) $validator->error()->add('email', 'The email address already exists!');
+
+            $admin_mobile_phone_exists = User::where('group_id', request('group_id')->where('mobile_phone', request('mobile_phone')))->count();
+            if ($admin_mobile_phone_exists) $validator->error()->add('mobile_phone', 'The Mobile Phone No already exists!');
+        });
+
+        if (!$validator->fails()) {
+            User::create(request()->all());
+
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->getMessageBag()->toArray()
+            ]);
+        }
     }
 }
